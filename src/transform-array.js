3d4df1ec6927a347d030d81b22f3564d274bdc4f
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-09-16 16:42:34 +0300

    feat: solve transform-arrays

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!');
+  if (arr.length === 0) return [];
+
+    let arrResult = [];
+    const len = arr.length;
+    let del = 0;
+
+    for (let i = 0; i < len; i++) {
+        
+        if (arr[i] === '--discard-next') {
+            if (i + 1 < len) {i++; del = 1};
+        } else
+        if (arr[i] === '--discard-prev') {
+            if (i - 1 >= 0 && del === 0) { arrResult.pop()}
+        } else
+        if (arr[i] === '--double-next') {
+            if (i + 1 < len) {arrResult.push(arr[i+1], arr[i+1]); i++}
+        } else
+        if (arr[i] === '--double-prev') {
+            if (i - 1 >= 0 && del === 0) arrResult.push(arr[i-1])
+        } else {
+            arrResult.push(arr[i]);
+        }
+        
+    }
+
+
+    return arrResult;
+
 }
